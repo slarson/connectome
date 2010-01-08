9bb116baeb08f15f0eae6e6eb32c191ddcd3e32b
mozety12 <mozety12@localhost>
2010-01-08 01:52:28 +0000

    [maven-scm] copy for tag mcb-0.0.3

---
+++ mcb/tags/mcb-0.0.3/src/main/java/Link.java
@@ -0,0 +1,35 @@
+
+
+import org.apache.commons.collections15.Factory;
+
+/**
+ * The class is used to create the edges for the graph.  This class is implemented 
+ * by Multi-Scale Connectome Browser.
+ * @date    December 10, 2009
+ * @author  Ruggero Carloz
+ * @version 0.0.2
+ */
+public class Link implements Factory{
+
+	/*
+	 * Constructor
+	 */
+	public Link(){}
+	/*
+	 * Method makes the edges value printable.
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		return " ";
+	}
+
+	public Object create() {
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		return new Link();
+	}
+}
