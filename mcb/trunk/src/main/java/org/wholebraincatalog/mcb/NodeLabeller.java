d95b763418f64382d67eddae3867bbe399942586
stephen larson <stephen.larson@gmail.com>
2010-05-19 11:08:06 +0000

    Refactors the NodeLabeller to perhaps make it more clear where the ClassCastException is coming from

--- mcb/trunk/src/main/java/org/wholebraincatalog/mcb/NodeLabeller.java
+++ mcb/trunk/src/main/java/org/wholebraincatalog/mcb/NodeLabeller.java
@@ -3,36 +3,44 @@ package org.wholebraincatalog.mcb;
 import java.util.HashMap;
 import java.util.Set;
 
+import org.apache.commons.collections15.Transformer;
+
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
-public class NodeLabeller extends ToStringLabeller{
+public class NodeLabeller<V> implements Transformer<V, String> {
 
-	/* (non-Javadoc)
-	 * @see edu.uci.ics.jung.visualization.decorators.DefaultToolTipFunction#getToolTipText(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeedu.uci.ics.jung.visualization.decorators.DefaultToolTipFunction#
+	 * getToolTipText(java.lang.Object)
 	 */
-	@Override
-	public String transform(Object v) {
-		String cell="NOTHING";
+	public String transform(V v) {
+
+		Node n = (Node) v;
+
+		String cell = "NOTHING";
 		String neurotransmitter;
 		String role;
 		String data_str = null;
-		
-		for(String key : ((Set<String>)(((Node)v).getNodeCellsMap().keySet()))){
+
+		for (String key : n.getNodeCellsMap().keySet()) {
 			System.out.println(cell);
-			for(String cellName : ((Node)v).getNodeCellsMap().get(key)
-					.getNeurotransmitterData().keySet()){
+			for (String cellName : ((Node) v).getNodeCellsMap().get(key)
+					.getNeurotransmitterData().keySet()) {
 				cell = cellName;
-				for(NeurotransmitterData data :((Node)v).getNodeCellsMap().
-						get(key).getNeurotransmitter(cellName)){
+				for (NeurotransmitterData data : ((Node) v).getNodeCellsMap()
+						.get(key).getNeurotransmitter(cellName)) {
 					neurotransmitter = data.getNeurotransmitter();
 					role = data.getRole();
-					data_str += cell+" "+neurotransmitter+" "+role+"\n";
+					data_str += cell + " " + neurotransmitter + " " + role
+							+ "\n";
 				}
 			}
 		}
-
 		return data_str;
+
 	}
 
 }
