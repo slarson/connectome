6f93bd923f478d0534c2fbe8e9e1514568769db4
stephen larson <stephen.larson@gmail.com>
2010-05-20 01:26:32 +0000

    Attempt to add meaningful hyperlink to edge label.  unfortunately the edge label goes away when you try to click it...

--- mcb/trunk/src/main/java/org/wholebraincatalog/mcb/EdgeLabeller.java
+++ mcb/trunk/src/main/java/org/wholebraincatalog/mcb/EdgeLabeller.java
@@ -2,14 +2,17 @@ package org.wholebraincatalog.mcb;
 
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
-public class MyLabeller extends ToStringLabeller{
+public class EdgeLabeller extends ToStringLabeller<Edge>{
 
 	/* (non-Javadoc)
 	 * @see edu.uci.ics.jung.visualization.decorators.DefaultToolTipFunction#getToolTipText(java.lang.Object)
 	 */
 	@Override
-	public String transform(Object v) {
-		return ((Edge)v).getReference();
+	public String transform(Edge v) {
+		String out = "";
+		String reference = v.getReference();
+		out += "<a href=\"http://" + reference + "\">" + reference + "</a>";
+		return out;
 	}
 
 }
