ef63b23352ba0f2eaa010d1213ef479c8b2334b3
mozety12 <mozety12@localhost>
2010-06-01 19:09:25 +0000

    Reduced brain region names in order to reduce query url.

--- mcb/trunk/src/main/java/org/wholebraincatalog/mcb/CellDataLoader.java
+++ mcb/trunk/src/main/java/org/wholebraincatalog/mcb/CellDataLoader.java
@@ -66,90 +66,94 @@ public class CellDataLoader {
 		String part_suffix = "_p";
 		String neurotransmitter_suffix = "_n";
 		String transmitter_role_suffix = "_t_r";
-		String[] brainRegions =  reduceBrainRegionName(brainRegionNames);
+		String brainRegionSufixName = null;
 
 		for(String RegionName : brainRegionNames){
-			for (String brainRegionName : brainRegions){
 
-				drb.addQueryTriplet("$" + brainRegionName + region_suffix + 
-						" <http://semantic-mediawiki.org/swivt/1.0#page> " + 
-						" <http://neurolex.org/wiki/Category:"+
-						RegionName+">");
+			if(brainRegionSufixName == null)
+				brainRegionSufixName =  reduceBrainRegionName(RegionName);
 
-				drb.addQueryTriplet("$"+brainRegionName+cells_suffix +
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALocated_in> $" +
-						brainRegionName + region_suffix );
+			drb.addQueryTriplet("$" + brainRegionSufixName + region_suffix + 
+					" <http://semantic-mediawiki.org/swivt/1.0#page> " + 
+					" <http://neurolex.org/wiki/Category:"+
+					RegionName+">");
 
-				drb.addQueryTriplet("$"+brainRegionName+cells_suffix+
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
-						brainRegionName+"_cells_label");
+			drb.addQueryTriplet("$"+brainRegionSufixName+cells_suffix +
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALocated_in> $" +
+					brainRegionSufixName + region_suffix );
 
-				drb.addQueryTriplet("$"+brainRegionName+cells_suffix+
-						" <http://semantic-mediawiki.org/swivt/1.0#page> $"+
-						brainRegionName+"_cells_url");
+			drb.addQueryTriplet("$"+brainRegionSufixName+cells_suffix+
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
+					brainRegionSufixName+"_cl");
 
-				drb.addQueryTriplet("$"+brainRegionName+cells_suffix
-						+" <http://neurolex.org/wiki/Special:URIResolver/Property-3AHas_role>" +
-				" <http://neurolex.org/wiki/Special:URIResolver/Category-3APrincipal_neuron_role>");
+			drb.addQueryTriplet("$"+brainRegionSufixName+cells_suffix+
+					" <http://semantic-mediawiki.org/swivt/1.0#page> $"+
+					brainRegionSufixName+"_cu");
 
-				drb.addQueryTriplet("$"+brainRegionName+cells_suffix +
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3ANeurotransmitter> $" +
-						brainRegionName+neurotransmitter_suffix);
+			drb.addQueryTriplet("$"+brainRegionSufixName+cells_suffix
+					+" <http://neurolex.org/wiki/Special:URIResolver/Property-3AHas_role>" +
+			" <http://neurolex.org/wiki/Special:URIResolver/Category-3APrincipal_neuron_role>");
 
-				drb.addQueryTriplet("$"+brainRegionName+neurotransmitter_suffix +
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $" +
-						brainRegionName+"_neurotransmitter_label");
+			drb.addQueryTriplet("$"+brainRegionSufixName+cells_suffix +
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3ANeurotransmitter> $" +
+					brainRegionSufixName+neurotransmitter_suffix);
 
-				drb.addQueryTriplet("$"+brainRegionName+neurotransmitter_suffix +
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3AHas_role> $"+
-						brainRegionName+transmitter_role_suffix);
+			drb.addQueryTriplet("$"+brainRegionSufixName+neurotransmitter_suffix +
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $" +
+					brainRegionSufixName+"_nl");
 
-				drb.addQueryTriplet("$"+brainRegionName+transmitter_role_suffix +
-						"<http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
-						brainRegionName+"_transmitter_role_label");
+			drb.addQueryTriplet("$"+brainRegionSufixName+neurotransmitter_suffix +
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3AHas_role> $"+
+					brainRegionSufixName+transmitter_role_suffix);
 
-				drb.addQueryTriplet("$"+brainRegionName+region_suffix +
-						" <http://neurolex.org/wiki/Special:URIResolver/Property-3AIs_part_of> $"+
-						brainRegionName+part_suffix);
+			drb.addQueryTriplet("$"+brainRegionSufixName+transmitter_role_suffix +
+					"<http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
+					brainRegionSufixName+"_trl");
 
-				drb.addQueryTriplet("$"+brainRegionName+part_suffix+" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
-						brainRegionName+"_is_part_of");
+			drb.addQueryTriplet("$"+brainRegionSufixName+region_suffix +
+					" <http://neurolex.org/wiki/Special:URIResolver/Property-3AIs_part_of> $"+
+					brainRegionSufixName+part_suffix);
 
-				drb.addSelectVariable("$"+ brainRegionName + "_cl");
-				drb.addSelectVariable("$"+ brainRegionName + "_cu");
-				drb.addSelectVariable("$"+ brainRegionName + "_nl");
-				drb.addSelectVariable("$"+ brainRegionName + "_trl");
-				drb.addSelectVariable("$"+ brainRegionName + "_ipo");
+			drb.addQueryTriplet("$"+brainRegionSufixName+part_suffix+" <http://neurolex.org/wiki/Special:URIResolver/Property-3ALabel> $"+
+					brainRegionSufixName+"_is_part_of");
 
-				//add union between all sets of variables except the last
-				if (brainRegionName.equals(brainRegionNames[brainRegionNames.length - 1]) == false) {
-					drb.addQueryTriplet("} UNION {");
-				}
+			drb.addSelectVariable("$"+ brainRegionSufixName + "_cl");
+			drb.addSelectVariable("$"+ brainRegionSufixName + "_cu");
+			drb.addSelectVariable("$"+ brainRegionSufixName + "_nl");
+			drb.addSelectVariable("$"+ brainRegionSufixName + "_trl");
+			drb.addSelectVariable("$"+ brainRegionSufixName + "_ipo");
+
+			//add union between all sets of variables except the last
+			if (RegionName.equals(brainRegionNames[brainRegionNames.length - 1]) == false) {
+				drb.addQueryTriplet("} UNION {");
 			}
+			brainRegionSufixName = null;
 		}
+
 	}
 	/**
 	 * Method reduces the name of the brain regions.
-	 * @param brainRegionNames - array containing the name regions.
-	 * @return - array with reduced brain region names.
+	 * @param brainRegionName - full name of the brain region.
+	 * @return reducedName - reduced name of the brain region.
 	 */
-	public static String[] reduceBrainRegionName(String[] brainRegionNames){
+	public static String reduceBrainRegionName(String brainRegionName){
 
-		String[] brainRegionsReducedNames = new String[brainRegionNames.length];
-		String dumName;
-		int i = 0;
+		String reducedName;
 		int index = 0;
-		for(String brainRegionName: brainRegionNames){
-			dumName = brainRegionName.substring(0, 0);
-			while(brainRegionName.indexOf('_') != -1){
-				index = brainRegionName.indexOf('_')+1;
-				dumName+=brainRegionName.charAt(index);
-				brainRegionName = brainRegionName.substring(index+1);	
-			}
-			brainRegionsReducedNames[i] = dumName;
-			i++;
+		
+		//obtain first letter of brain region.
+		reducedName = brainRegionName.substring(0, 1);
+		
+		//loop and obtain the first letter of brain region after 
+		// the underscore.
+		while(brainRegionName.indexOf('_') != -1){
+			index = brainRegionName.indexOf('_')+1;
+			reducedName+=brainRegionName.charAt(index);
+			System.out.println("dumName: "+reducedName.toLowerCase());
+			brainRegionName = brainRegionName.substring(index);	
 		}
-		return brainRegionsReducedNames;
+		
+		return reducedName.toLowerCase();
 	}
 	/**
 	 * Method searches for the cell data that corresponds to a given node and 
