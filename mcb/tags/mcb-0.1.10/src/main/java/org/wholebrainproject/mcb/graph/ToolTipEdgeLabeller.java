a6214e1bbc91c7c65e74a28e341a5d8414282335
stephen.larson <stephen.larson@localhost>
2010-06-15 17:57:46 +0000

    [maven-scm] copy for tag mcb-0.1.10

---
+++ mcb/tags/mcb-0.1.10/src/main/java/org/wholebrainproject/mcb/graph/ToolTipEdgeLabeller.java
@@ -0,0 +1,14 @@
+package org.wholebrainproject.mcb.graph;
+
+
+import org.apache.commons.collections15.Transformer;
+
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+
+public class ToolTipEdgeLabeller implements Transformer<Edge, String>{
+
+	public String transform(Edge input) {
+		return input.getToolTipLabel();
+	}
+
+}
