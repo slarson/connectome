22c8a69c4ad163172e2c59dcf29337f863bf9d92
stephen.larson <stephen.larson@localhost>
2010-06-16 10:19:27 +0000

    [maven-scm] copy for tag mcb-0.1.13

---
+++ mcb/tags/mcb-0.1.13/src/main/java/org/wholebrainproject/mcb/graph/HasCellEdge.java
@@ -0,0 +1,59 @@
+package org.wholebrainproject.mcb.graph;
+
+import java.awt.BasicStroke;
+import java.awt.Font;
+
+/**
+ * Edge that defines a connection between a brain region node and a cell node.
+ * Indicates that the brain region has the cell type indiciated by the node.
+ * @author slarson
+ *
+ */
+public class HasCellEdge implements Edge {
+
+	public Number getCloseness() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Font getFont() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getMoreDetailsURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getReferenceURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BasicStroke getStroke() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getToolTipLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getInferenceChain() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasInferenceChain() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
