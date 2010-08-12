3962bd68b2b4077d949e073e14ad0eec279609ae
mozety12 <mozety12@localhost>
2010-08-12 21:10:08 +0000

    Edges have rectangles instead of arrows. Removed ArrowColor class since it is not needed.

--- mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/graph/ArrowTransform.java
+++ mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/graph/ArrowTransform.java
@@ -1,22 +1,27 @@
 package org.wholebrainproject.mcb.graph;
 
-
-
-
+import java.awt.Rectangle;
 import java.awt.Shape;
 
 import org.apache.commons.collections15.Transformer;
+
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.Context;
-import edu.uci.ics.jung.visualization.util.ArrowFactory;
+/**
+ * This class handles the shape of the edge's arrows. By default directed edges
+ * mark the edge's direction with an arrow.  This class changes the shape of the
+ * default arrow to a rectangle.     
+ * @author Ruggero Carloz
+ * @date 8-12-2010
+ */
+public class ArrowTransform implements Transformer<Context<Graph<Node,Edge>,Edge>,Shape> {
 
-public class ArrowTransform implements Transformer<Context<Graph<Node,Edge>,Edge>,Shape>{
+    public Shape transform(Context<Graph<Node, Edge>, Edge> input) {
+        //arrow to be drawn in edge.
+        // this functionality might enable us to indicate
+        // types of connectivity among brain regions.
+        Rectangle rec = new Rectangle(-5,-5,6,10);
+        return rec;
+    }
 
-	public Shape transform(Context<Graph<Node, Edge>, Edge> input) {
-		Graph g = GraphManager.getInstance().getGraph();
-		ArrowFactory arrow = new ArrowFactory();
-        return arrow.getNotchedArrow(10, 10, 0);
-	}
-
-	
 }
