56b0875327b6cd7051d83219f576dbc4d0d602c2
mozety12 <mozety12@localhost>
2010-09-30 19:32:03 +0000

    Made some comments.

--- mergingrdfs/src/main/java/ExpandAndWriteIntersection.java
+++ mergingrdfs/src/main/java/ExpandAndWriteIntersection.java
@@ -3,23 +3,38 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-
+/**
+ * This class takes care of merging the data obtained from the 
+ * BAMS rdf and the neurolex rdf.  After the date is merged it
+ * is compared in order to find the intersection between the 
+ * rdf documents.  Once the intersection is found the result
+ * is printed to file "regions_matched_with_BAMS_and_neurolex.txt"
+ * @author ruggero carloz
+ * @date 09-29-2010
+ */
 
 public class ExpandAndWriteIntersection {
-	
+
 	private static ExpandAndWriteIntersection instance = null;
 	private static HashMap<Integer,NeurolexPageId> bamsRegions;
 	private static HashMap<Integer,brainRegionSynonyms> neurolexSynonyms;
 	private static HashMap<Integer,NeurolexPageId> neurolexNoSynonyms;
 	private static HashMap<Integer, NeurolexPageId> completeData;
-	
+
 	public static ExpandAndWriteIntersection getInstance(){
 		if(instance == null){
 			instance = new ExpandAndWriteIntersection();
 		}
 		return instance;
 	}
-	
+
+	/**
+	 * Method takes care of setting the global variables.
+	 * @param bamsData
+	 * @param hashCodeNeurolex
+	 * @param neurolexWithNoSynonyms
+	 * @param data
+	 */
 	public void setData(HashMap<Integer,NeurolexPageId> bamsData,HashMap<Integer,brainRegionSynonyms> hashCodeNeurolex,
 			HashMap<Integer,NeurolexPageId> neurolexWithNoSynonyms,HashMap<Integer, NeurolexPageId> data){
 		this.bamsRegions = bamsData;
@@ -27,13 +42,13 @@ public class ExpandAndWriteIntersection {
 		this.neurolexNoSynonyms = neurolexWithNoSynonyms;
 		this.completeData = data;
 	}
-	
+
 	/**
 	 * Method merges the data that belongs to brain regions that
 	 * have synonyms with the rest of the data.
 	 */
 	static void expandDataWithSynonyms() {
-		
+
 		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
 
 		for(Integer key: completeData.keySet()){
@@ -48,7 +63,7 @@ public class ExpandAndWriteIntersection {
 		}
 		expandDataWithNoSynonyms();	
 	}
-	
+
 	/**
 	 * Method merges the data that belongs to brain regions that
 	 * have no synonyms with the rest of the data that contains
@@ -57,7 +72,7 @@ public class ExpandAndWriteIntersection {
 	private static void expandDataWithNoSynonyms() {
 
 		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
-		
+
 		for(Integer key: completeData.keySet()){
 			if(neurolexNoSynonyms.containsKey(key)){
 				dataDummy.put(key, neurolexNoSynonyms.get(key));					
@@ -66,7 +81,7 @@ public class ExpandAndWriteIntersection {
 		for(Integer key: dataDummy.keySet()){
 			completeData.put(key, dataDummy.get(key));
 		}
-		
+
 	}
 
 	/**
@@ -81,7 +96,7 @@ public class ExpandAndWriteIntersection {
 				completeData.get(key).getPage(),completeData.get(key).getId());
 
 	}
-	
+
 	/**
 	 * Method gives the hash code of a given word.
 	 * @param synonym
@@ -93,12 +108,12 @@ public class ExpandAndWriteIntersection {
 	}
 
 	/**
-	 * Method finds the intersection between the BAMS data and Neurolex
+	 * Method finds the intersection between the BAMS data and neurolex
 	 * and prints the set to file. 
 	 * @throws IOException
 	 */
 	static void findMatchesAndWrite() throws IOException{
-		File file = new File("/Users/rcarloz/Desktop/regions_matched_with_BAMS.txt");
+		File file = new File("/Users/rcarloz/Desktop/regions_matched_with_BAMS_and_neurolex.txt");
 		FileOutputStream fos = new FileOutputStream(file);
 		DataOutputStream out=new DataOutputStream(fos);
 
@@ -151,7 +166,7 @@ public class ExpandAndWriteIntersection {
 	 * @return
 	 */
 	private static String getDescription(NeurolexPageId neurolexPageId) {
-		
+
 		return neurolexPageId.getDescription().replace(",", "");
 	}
 
@@ -162,7 +177,7 @@ public class ExpandAndWriteIntersection {
 	 */
 	private static String getName(NeurolexPageId neurolexPageId) {
 		return neurolexPageId.getName().replace(",","" );
-		
+
 	}
 
 
