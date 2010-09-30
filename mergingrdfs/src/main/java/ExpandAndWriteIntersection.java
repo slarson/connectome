834d9deaf69e701d6ee1d3f8297178c40395419f
mozety12 <mozety12@localhost>
2010-09-30 19:13:17 +0000

    Made the core more readable.

---
+++ mergingrdfs/src/main/java/ExpandAndWriteIntersection.java
@@ -0,0 +1,169 @@
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class ExpandAndWriteIntersection {
+	
+	private static ExpandAndWriteIntersection instance = null;
+	private static HashMap<Integer,NeurolexPageId> bamsRegions;
+	private static HashMap<Integer,brainRegionSynonyms> neurolexSynonyms;
+	private static HashMap<Integer,NeurolexPageId> neurolexNoSynonyms;
+	private static HashMap<Integer, NeurolexPageId> completeData;
+	
+	public static ExpandAndWriteIntersection getInstance(){
+		if(instance == null){
+			instance = new ExpandAndWriteIntersection();
+		}
+		return instance;
+	}
+	
+	public void setData(HashMap<Integer,NeurolexPageId> bamsData,HashMap<Integer,brainRegionSynonyms> hashCodeNeurolex,
+			HashMap<Integer,NeurolexPageId> neurolexWithNoSynonyms,HashMap<Integer, NeurolexPageId> data){
+		this.bamsRegions = bamsData;
+		this.neurolexSynonyms = hashCodeNeurolex;
+		this.neurolexNoSynonyms = neurolexWithNoSynonyms;
+		this.completeData = data;
+	}
+	
+	/**
+	 * Method merges the data that belongs to brain regions that
+	 * have synonyms with the rest of the data.
+	 */
+	static void expandDataWithSynonyms() {
+		
+		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
+
+		for(Integer key: completeData.keySet()){
+			if(neurolexSynonyms.containsKey(key)){
+				for(String synonym: neurolexSynonyms.get(key).getSynonyms()){
+					dataDummy.put(getHash(synonym), createNeurolexPageId(synonym,key));					
+				}
+			}
+		}
+		for(Integer key: dataDummy.keySet()){
+			completeData.put(key, dataDummy.get(key));
+		}
+		expandDataWithNoSynonyms();	
+	}
+	
+	/**
+	 * Method merges the data that belongs to brain regions that
+	 * have no synonyms with the rest of the data that contains
+	 * brain regions that have synonyms.
+	 */
+	private static void expandDataWithNoSynonyms() {
+
+		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
+		
+		for(Integer key: completeData.keySet()){
+			if(neurolexNoSynonyms.containsKey(key)){
+				dataDummy.put(key, neurolexNoSynonyms.get(key));					
+			}
+		}
+		for(Integer key: dataDummy.keySet()){
+			completeData.put(key, dataDummy.get(key));
+		}
+		
+	}
+
+	/**
+	 * Method creates a NeurolexPageId object from the synonyms to be 
+	 * stored in data.
+	 * @param synonym
+	 * @param key
+	 * @return
+	 */
+	private static NeurolexPageId createNeurolexPageId(String synonym, Integer key) {
+		return new NeurolexPageId(getHash(synonym),synonym,
+				completeData.get(key).getPage(),completeData.get(key).getId());
+
+	}
+	
+	/**
+	 * Method gives the hash code of a given word.
+	 * @param synonym
+	 * @return
+	 */
+	private static Integer getHash(String synonym) {
+		return synonym.replace(" ","").toLowerCase().hashCode();
+
+	}
+
+	/**
+	 * Method finds the intersection between the BAMS data and Neurolex
+	 * and prints the set to file. 
+	 * @throws IOException
+	 */
+	static void findMatchesAndWrite() throws IOException{
+		File file = new File("/Users/rcarloz/Desktop/regions_matched_with_BAMS.txt");
+		FileOutputStream fos = new FileOutputStream(file);
+		DataOutputStream out=new DataOutputStream(fos);
+
+		long start = System.currentTimeMillis();
+		out.writeBytes("Brain region name, Source, Species, Neurolex page, Neurolex id \n");
+		for(Integer bamsNameHash: bamsRegions.keySet()){
+			if(completeData.containsKey(bamsNameHash)){
+				out.writeBytes(getName(bamsRegions.get(bamsNameHash))+","
+						+getDescription(bamsRegions.get(bamsNameHash))+","+
+						getSpecies(bamsRegions.get(bamsNameHash))+
+						","+getPage(completeData.get(bamsNameHash))+","+getId(completeData.get(bamsNameHash))+"\n");
+			}
+		}
+		out.close();
+		long end = System.currentTimeMillis();
+		long total = end - start;
+		System.out.println("time to look for matches " + total + " ms");
+	}
+
+	/**
+	 * Method returns the id of the given brain region.
+	 * @param neurolexPageId
+	 * @return
+	 */
+	private static String getId(NeurolexPageId neurolexPageId) {
+		return neurolexPageId.getId();
+	}
+
+	/**
+	 * Method return the Neurolex page where the data can be found.
+	 * @param neurolexPageId
+	 * @return
+	 */
+	private static String getPage(NeurolexPageId neurolexPageId) {
+		return neurolexPageId.getPage();
+	}
+
+	/**
+	 * Method returns the name species to which the given data belongs too.
+	 * @param neurolexPageId
+	 * @return
+	 */
+	private static String getSpecies(NeurolexPageId neurolexPageId) {
+		return neurolexPageId.getSpecie();
+	}
+
+	/**
+	 * Method returns the formated description of a given brain region.
+	 * @param neurolexPageId
+	 * @return
+	 */
+	private static String getDescription(NeurolexPageId neurolexPageId) {
+		
+		return neurolexPageId.getDescription().replace(",", "");
+	}
+
+	/**
+	 * Method returns the formated name of a given brain region.
+	 * @param neurolexPageId
+	 * @return
+	 */
+	private static String getName(NeurolexPageId neurolexPageId) {
+		return neurolexPageId.getName().replace(",","" );
+		
+	}
+
+
+}
--- mergingrdfs/src/main/java/SetDisjoint.java
+++ mergingrdfs/src/main/java/SetDisjoint.java
@@ -23,160 +23,20 @@ public class SetDisjoint {
 	 * @author ruggero carloz
 	 * @date 08-24-10
 	 */
-	private static HashMap<Integer,NeurolexPageId> bamsRegions;
-	private static HashMap<Integer,brainRegionSynonyms> hashCodeNeurolex;
-	private static HashMap<Integer,NeurolexPageId> neurolexNoSynonyms;
-	private static HashMap<Integer, NeurolexPageId> data;
 	
-
 	public static void main(String[] args) throws IOException {
 
-		bamsRegions = RunQuery.RunBAMSQuery();
-		neurolexNoSynonyms = RunQuery.RunNeurolxQueryNoSynonyms();
-		hashCodeNeurolex = RunQuery.RunNeurolexQueryHashCode();
-		data = RunQuery.RunNeurolexQueryNamePageId();
-
-		expandDataWithSynonyms();
-
-		findMatchesAndWrite();
-
-	}
-	
-	/**
-	 * Method merges the data that belongs to brain regions that
-	 * have synonyms with the rest of the data.
-	 */
-	private static void expandDataWithSynonyms() {
+		HashMap<Integer,NeurolexPageId> bamsRegions = RunQuery.RunBAMSQuery();
+		HashMap<Integer,NeurolexPageId> neurolexNoSynonyms = RunQuery.RunNeurolxQueryNoSynonyms();
+		HashMap<Integer,brainRegionSynonyms> hashCodeNeurolex = RunQuery.RunNeurolexQueryHashCode();
+		HashMap<Integer, NeurolexPageId> data = RunQuery.RunNeurolexQueryNamePageId();
 		
-		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
-
-		for(Integer key: data.keySet()){
-			if(hashCodeNeurolex.containsKey(key)){
-				for(String synonym: hashCodeNeurolex.get(key).getSynonyms()){
-					dataDummy.put(getHash(synonym), createNeurolexPageId(synonym,key));					
-				}
-			}
-		}
-		for(Integer key: dataDummy.keySet()){
-			data.put(key, dataDummy.get(key));
-		}
-		expandDataWithNoSynonyms();	
-	}
-	
-	/**
-	 * Method merges the data that belongs to brain regions that
-	 * have no synonyms with the rest of the data that contains
-	 * brain regions that have synonyms.
-	 */
-	private static void expandDataWithNoSynonyms() {
-
-		HashMap<Integer, NeurolexPageId> dataDummy = new HashMap<Integer, NeurolexPageId>();
+		ExpandAndWriteIntersection.getInstance().setData(bamsRegions, hashCodeNeurolex, neurolexNoSynonyms, data);
 		
-		for(Integer key: data.keySet()){
-			if(neurolexNoSynonyms.containsKey(key)){
-				dataDummy.put(key, neurolexNoSynonyms.get(key));					
-			}
-		}
-		for(Integer key: dataDummy.keySet()){
-			data.put(key, dataDummy.get(key));
-		}
-		
-	}
+		ExpandAndWriteIntersection.getInstance().expandDataWithSynonyms();
 
-	/**
-	 * Method creates a NeurolexPageId object from the synonyms to be 
-	 * stored in data.
-	 * @param synonym
-	 * @param key
-	 * @return
-	 */
-	private static NeurolexPageId createNeurolexPageId(String synonym, Integer key) {
-		return new NeurolexPageId(getHash(synonym),synonym,
-				data.get(key).getPage(),data.get(key).getId());
+		ExpandAndWriteIntersection.getInstance().findMatchesAndWrite();
 
-	}
-	
-	/**
-	 * Method gives the hash code of a given word.
-	 * @param synonym
-	 * @return
-	 */
-	private static Integer getHash(String synonym) {
-		return synonym.replace(" ","").toLowerCase().hashCode();
-
-	}
-
-	/**
-	 * Method finds the intersection between the BAMS data and Neurolex
-	 * and prints the set to file. 
-	 * @throws IOException
-	 */
-	private static void findMatchesAndWrite() throws IOException{
-		File file = new File("/Users/rcarloz/Desktop/regions_matched_with_BAMS.txt");
-		FileOutputStream fos = new FileOutputStream(file);
-		DataOutputStream out=new DataOutputStream(fos);
-
-		long start = System.currentTimeMillis();
-		out.writeBytes("Brain region name, Source, Species, Neurolex page, Neurolex id \n");
-		for(Integer bamsNameHash: bamsRegions.keySet()){
-			if(data.containsKey(bamsNameHash)){
-				out.writeBytes(getName(bamsRegions.get(bamsNameHash))+","
-						+getDescription(bamsRegions.get(bamsNameHash))+","+
-						getSpecies(bamsRegions.get(bamsNameHash))+
-						","+getPage(data.get(bamsNameHash))+","+getId(data.get(bamsNameHash))+"\n");
-			}
-		}
-		out.close();
-		long end = System.currentTimeMillis();
-		long total = end - start;
-		System.out.println("time to look for matches " + total + " ms");
-	}
-
-	/**
-	 * Method returns the id of the given brain region.
-	 * @param neurolexPageId
-	 * @return
-	 */
-	private static String getId(NeurolexPageId neurolexPageId) {
-		return neurolexPageId.getId();
-	}
-
-	/**
-	 * Method return the Neurolex page where the data can be found.
-	 * @param neurolexPageId
-	 * @return
-	 */
-	private static String getPage(NeurolexPageId neurolexPageId) {
-		return neurolexPageId.getPage();
-	}
-
-	/**
-	 * Method returns the name species to which the given data belongs too.
-	 * @param neurolexPageId
-	 * @return
-	 */
-	private static String getSpecies(NeurolexPageId neurolexPageId) {
-		return neurolexPageId.getSpecie();
-	}
-
-	/**
-	 * Method returns the formated description of a given brain region.
-	 * @param neurolexPageId
-	 * @return
-	 */
-	private static String getDescription(NeurolexPageId neurolexPageId) {
-		
-		return neurolexPageId.getDescription().replace(",", "");
-	}
-
-	/**
-	 * Method returns the formated name of a given brain region.
-	 * @param neurolexPageId
-	 * @return
-	 */
-	private static String getName(NeurolexPageId neurolexPageId) {
-		return neurolexPageId.getName().replace(",","" );
-		
 	}
 
 }
