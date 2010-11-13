ebef0b736e172c8434185b138dc97365db23b4a0
caprea <caprea@localhost>
2010-11-13 21:13:59 +0000

    Added a Toolbar, removed the auto-pop-up of the instructions panel and also made the code less redundant in the initialization. There were two ways to start the app and it was duplicated code (for instance, turning off the popup had to be done in two places) I combined them so the startup is the same for both ways (Applet and Application)

---
+++ mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/ToolBar.java
@@ -0,0 +1,101 @@
+package org.wholebrainproject.mcb;
+
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class ToolBar extends JToolBar
+{
+	
+	
+	private MouseListener toolBarListener = null;
+	
+	
+	
+	ToolBarButton btnClear = new ToolBarButton(null, "Clear Regions");
+	ToolBarButton btnAdd = new ToolBarButton(null, "Add Region");
+	
+	protected MouseListener createListener()
+	{
+			MouseListener listener = new MouseListener() {
+			
+			public void mouseReleased(MouseEvent e) 
+			{
+				//pass the event to our actual handler
+				if ( e.getSource() != null && e.getSource() instanceof ToolBarButton)
+				{
+					executeCommand((ToolBarButton) e.getSource());
+				}
+				
+			}
+			
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+
+		return listener;
+	}
+	
+	
+	protected void executeCommand(ToolBarButton sourceButton)
+	{
+		if ( sourceButton == btnAdd)
+		{
+			System.out.println("Add Button pressed");
+		}
+		else if ( sourceButton == btnClear)
+		{
+			System.out.println("Clear Button Pressed");
+		}
+			
+	}
+	
+	public ToolBar() 
+	{
+		toolBarListener = createListener();
+		
+		
+		add(btnAdd);
+		btnAdd.addMouseListener(toolBarListener);
+		
+		add(btnClear);
+		btnClear.addMouseListener(toolBarListener);
+	}
+	
+	class ToolBarButton extends JButton
+	{
+		public ToolBarButton(Icon icon, String text) 
+		{
+			super(icon);
+			
+			setVerticalTextPosition(BOTTOM);
+			setHorizontalTextPosition(CENTER);
+			setText(text); //dont put a label ont he buttons
+			setActionCommand(text);
+			setToolTipText(text);
+		}	
+	}
+
+}
