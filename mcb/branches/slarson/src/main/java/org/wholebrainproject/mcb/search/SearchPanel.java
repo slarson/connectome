7e7db5018fe7e95a781f56416f41e5a4e484325e
mozety12 <mozety12@localhost>
2010-12-08 13:55:21 -0800

    Added some comments.  Integrated mouse listener for search panel.

--- mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/search/SearchPanel.java
+++ mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/search/SearchPanel.java
@@ -1,14 +1,14 @@
 package org.wholebrainproject.mcb.search;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -18,21 +18,25 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import org.jdesktop.swingx.combobox.ListComboBoxModel;
-import org.wholebrainproject.mcb.View;
+import org.wholebrainproject.mcb.ToolBar.ToolBarButton;
 import org.wholebrainproject.mcb.data.BAMSToNeurolexMap;
+import org.wholebrainproject.mcb.graph.GraphManager;
 
 /**
  * A panel to pick from a list of brain regions that are available to put on the
  * connectome browser graph.
- * 
+ *
  * @author slarson
- * 
+ *
  */
 public class SearchPanel extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	private MouseListener mouseListener = null;
 
 	private List<String> names;
 
@@ -41,12 +45,17 @@ public class SearchPanel extends JDialog {
 	/**
 	 * {@inheritDoc}
 	 */
-	
+
 	JButton btnOK = new JButton("OK");
 	JButton btnCancel = new JButton("Cancel");
-	
-	
-	public SearchPanel(JFrame owner) 
+
+
+	/**
+	 * Method creates the search panel and add the brain regions
+	 * from master list to it.
+	 * @param owner
+	 */
+	public SearchPanel(JFrame owner)
 	{
 		super(owner);
 		setModal(true);
@@ -54,6 +63,8 @@ public class SearchPanel extends JDialog {
 
 		GridBagConstraints gridBagConstraints;
 
+		//mouse listener.
+		mouseListener = createListener();
 		strictComboBox = new JComboBox();
 
 		JLabel strictComboBoxLabel = new JLabel();
@@ -68,32 +79,108 @@ public class SearchPanel extends JDialog {
 		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		gridBagConstraints.insets = new Insets(3, 3, 3, 3);
 		add(strictComboBox, gridBagConstraints);
-		
+
+		//set the coordinates for the buttons used combo box.
 		GridBagConstraints buttonConstraints = new GridBagConstraints();
 		buttonConstraints.gridwidth = GridBagConstraints.NONE;
 		buttonConstraints.fill = GridBagConstraints.HORIZONTAL;
 		buttonConstraints.insets = new Insets(1, 3, 3, 3);
-		
+
+		//adding mouse listeners to buttons.
+		btnOK.addMouseListener(mouseListener);
+		btnCancel.addMouseListener(mouseListener);
+
+		//add buttons to combo box.
 		add(btnOK, buttonConstraints);
 		add(btnCancel, buttonConstraints);
-		
+
 		Dimension size = new Dimension(500, 250);
-		
+
 		setMinimumSize(size);
 		setSize(size);
 		setPreferredSize(size);
-		
+
 		injectResources();
 		bind();
 		decorate();
 		repaint();
-		
+
+	}
+	/**
+	 * Method takes care of the mouse listener for the tool bar.
+	 * @return
+	 */
+	protected MouseListener createListener()
+	{
+			MouseListener listener = new MouseListener() {
+
+			public void mouseReleased(MouseEvent e)
+			{
+				//pass the event to our actual handler
+				if ( e.getSource() != null && e.getSource() instanceof JButton)
+				{
+					executeCommand((JButton) e.getSource());
+				}
+
+			}
+
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		};
+
+		return listener;
 	}
 
+	/**
+	 * Method checks which tool bar button was clicked and
+	 * acts accordingly.
+	 * @param sourceButton
+	 */
+	protected void executeCommand(JButton sourceButton)
+	{
+		//button to add brain region was clicked.
+		if ( sourceButton == btnOK)
+		{
+			//showSearch();
+		}
+		//button to clear the graph was clicked.
+		else if ( sourceButton == btnCancel)
+		{
+			System.out.println("Close box.");
+
+		}
+
+	}
+	/**
+	 * Method gets the brain region names form the master list and
+	 * adds them to a map.  The list is sorted by alphabetical order.
+	 */
 	protected void injectResources() {
 		try {
+			//get brain region names from master list.
 			Map<String, URI> namesMap = BAMSToNeurolexMap.getInstance().getNamesMap();
-			names = new ArrayList(namesMap.keySet());
+
+			//put the brain region names in an array.
+			names = new ArrayList<String>(namesMap.keySet());
+
+			//sort the brain region names by name.
 			Collections.sort(names);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -101,10 +188,18 @@ public class SearchPanel extends JDialog {
 		}
 	}
 
+	/**
+	 * Method adds the brain region names in the master list to the
+	 * combo box.
+	 */
 	protected void bind() {
+		//adding the brain region names to the combo box.
 		strictComboBox.setModel(new ListComboBoxModel<String>(names));
 	}
 
+	/**
+	 * Method decorates the combo box that contains the brain region names.
+	 */
 	private void decorate() {
 		AutoCompleteDecorator.decorate(strictComboBox);
 	}
