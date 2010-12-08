757cf093c795286ff4f1d46bc864e5809b118087
mozety12 <mozety12@localhost>
2010-12-08 13:55:21 -0800

    Added some comments.  Integrated mouse listener for search panel.

--- mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/ToolBar.java
+++ mcb/branches/slarson/src/main/java/org/wholebrainproject/mcb/ToolBar.java
@@ -1,17 +1,10 @@
 package org.wholebrainproject.mcb;
 
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Insets;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.Icon;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 
 import org.wholebrainproject.mcb.graph.GraphManager;
@@ -19,119 +12,148 @@ import org.wholebrainproject.mcb.search.SearchPanel;
 
 public class ToolBar extends JToolBar
 {
-	
-	
+	private static final long serialVersionUID = 1L;
+
 	private MouseListener toolBarListener = null;
-	
-	
-	
+
 	ToolBarButton btnClear = new ToolBarButton(null, "Clear Regions");
 	ToolBarButton btnAdd = new ToolBarButton(null, "Add Region");
 	SearchPanel pnlSearch = null;
-	
-	
+
+	/**
+	 * Method takes care of the mouse listener for the tool bar.
+	 * @return
+	 */
 	protected MouseListener createListener()
 	{
 			MouseListener listener = new MouseListener() {
-			
-			public void mouseReleased(MouseEvent e) 
+
+			public void mouseReleased(MouseEvent e)
 			{
 				//pass the event to our actual handler
 				if ( e.getSource() != null && e.getSource() instanceof ToolBarButton)
 				{
 					executeCommand((ToolBarButton) e.getSource());
 				}
-				
+
 			}
-			
+
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		};
 
 		return listener;
 	}
-	
-	
-	protected void executeCommand(ToolBarButton sourceButton)
+
+	/**
+	 * Method checks which tool bar button was clicked and
+	 * acts accordingly.
+	 * @param sourceButton
+	 */
+	public void executeCommand(ToolBarButton sourceButton)
 	{
+		//button to add brain region was clicked.
 		if ( sourceButton == btnAdd)
 		{
 			showSearch();
 		}
+		//button to clear the graph was clicked.
 		else if ( sourceButton == btnClear)
 		{
+			//get the graph and clear it.
 			GraphManager.getInstance().clearAllNodesAndEdges();
 		}
-			
+
 	}
-	
+
+	/**
+	 *
+	 */
 	private void showSearch()
 	{
+		System.out.println("Show search: ");
 		/*
 		Dimension size = new Dimension(500, 70);
 		JFrame frame = new JFrame("Search Panel");
 		frame.add(pnlSearch);
-		
+
 		frame.setPreferredSize(size);
 		frame.setMinimumSize(size);
 		frame.setSize(size);
 		frame.pack();
 		frame.setVisible(true);
-		*/
-		
-		pnlSearch.setVisible(true);
-		
-	}
-	
 
-	
-	public ToolBar() 
+		 */
+		pnlSearch.setVisible(true);
+
+	}
+
+	/**
+	 * Method instantiates the tool bar with it's appropriate
+	 * buttons.
+	 */
+	public ToolBar()
 	{
-		
+		//instantiate the seach panel.
 		pnlSearch = new SearchPanel(null);
-		
-		
+
+		//create mouse listener.
 		toolBarListener = createListener();
-		
-		
+
+		//adding the brain region add button to tool bar.
 		add(btnAdd);
 		btnAdd.addMouseListener(toolBarListener);
-		
+
+		//adding the clear button to tool bar.
 		add(btnClear);
 		btnClear.addMouseListener(toolBarListener);
+
 		this.setFloatable(false);
 		this.setRollover(true);
 	}
-	
-	class ToolBarButton extends JButton
+
+	/**
+	 * This class takes care of the tool bar buttons that enable
+	 * the user to,
+	 * 1.- Clear the current graph.
+	 * 2.- Add nodes to the graph by specifying a brain region
+	 * by pressing the add button.
+	 *
+	 */
+	public class ToolBarButton extends JButton
 	{
-		public ToolBarButton(Icon icon, String text) 
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Constructor for tool bar button.
+		 */
+		public ToolBarButton(Icon icon, String text)
 		{
 			super(icon);
-			
+
 			setVerticalTextPosition(BOTTOM);
 			setHorizontalTextPosition(CENTER);
 			setText(text); //dont put a label ont he buttons
 			setActionCommand(text);
 			setToolTipText(text);
-		}	
+		}
 	}
 
 }
