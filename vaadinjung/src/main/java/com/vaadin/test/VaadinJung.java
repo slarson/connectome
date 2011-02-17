01fa1a378fa82f9406cf1c2a39b1d9ffdded672b
mozety12 <mozety12@localhost>
2011-02-16 19:14:19 -0800

    Vaadin application that will implement GWT wrapper.

---
+++ vaadinjung/src/main/java/com/vaadin/test/VaadinJung.java
@@ -0,0 +1,34 @@
+package com.vaadin.test;
+
+import com.vaadin.Application;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Window.CloseEvent;
+import com.vaadin.ui.Window.CloseListener;
+/**
+ * Vaadin application intended to be implemented by the GWT wrapper
+ * in order port mcb test to the browser nativaly.
+ * @author ruggero
+ *
+ */
+public class VaadinJung extends Application{
+	TabSheet tabSheet = new TabSheet();
+
+	public void init(){
+		//set the title for the application window.
+		Window mainWindow = new Window("Vaadin Jung");
+		setMainWindow(mainWindow);
+		mainWindow.addComponent(tabSheet);
+
+		//make the application close and start fresh when
+		//window is closed.
+		getMainWindow().addListener(new CloseListener(){
+			public void windowClose(CloseEvent e) {
+				e.getWindow().removeAllComponents();
+				removeWindow(e.getWindow());
+				close();
+			}
+		});
+
+	}
+}
